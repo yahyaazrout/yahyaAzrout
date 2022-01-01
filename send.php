9e6f656bc85aabb0d983d2b98e75e2ead85830fc
yahyaazrout <yahiaazrout@gmail.com>
2022-01-01 16:47:50 +0100

    Portfolio

---
+++ send.php
@@ -0,0 +1,18 @@
+<?php
+if ($_POST["email"]) {
+    $response = $_POST["email"];
+//     $email = $_POST["email"];
+//     $to      = "yahiaazrout@gmail.com";
+//     $subject = "$subject";
+//     $message = "Hi, I'm '". $email ." 'from your Portfolio";
+//     $headers = 'From: azwebdeveloppement@gmail.com'       . "\r\n" .
+//                  'Reply-To:  azwebdeveloppement@gmail.com' . "\r\n" .
+//                  'Reply-To:  azwebdeveloppement@gmail.com' . "\r\n" ;
+// if (!empty($email) && $email != "") {
+//     $response = mail($to, $subject, $message, $headers);
+//     echo $response return;
+// }
+echo $response;
+}
+
+?>
